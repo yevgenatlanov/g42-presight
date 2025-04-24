ba9b4f01f079a4f114f083868eacffe485058f6c
Yevgen Atlanov <yevgen.atlanov@gmail.com>
2025-04-24 19:00:22 +0200

    vite env added

--- client/src/actions/api.ts
+++ client/src/actions/api.ts
@@ -6,7 +6,7 @@ import {
   UserFilter,
 } from "../types";
 
-const API_URL = process.env.REACT_APP_API_URL + "/api";
+const API_URL = (import.meta.env.VITE_API_URL || "") + "/api";
 
 const api = axios.create({
   baseURL: API_URL,
